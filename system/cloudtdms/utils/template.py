fe1902b64b0db1e0c117fd625a73f35eaa498328
Shahbaz Ali <shahbaz.ali03@gmail.com>
2020-07-09 20:35:18 +0530

    add attributes parsing and file generation

--- system/cloudtdms/utils/template.py
+++ system/cloudtdms/utils/template.py
@@ -13,6 +13,9 @@ from airflow.configuration import get_airflow_home
 from airflow.utils.log.logging_mixin import LoggingMixin
 sys.path.append(os.path.dirname(get_airflow_home()))
 from system.cloudtdms import providers
+from system.dags import get_providers_home
+from system.dags import get_output_data_home
+
 
 dag = DAG(
 dag_id={{ "'"~data.dag_id|string~"'" }},
@@ -26,7 +29,8 @@ dag_id={{ "'"~data.dag_id|string~"'" }},
             'retry_delay': timedelta(minutes=1)
         },
         params={
-            'stream': {{ data.stream }}
+            'stream': {{ data.stream }},
+            'attributes': {{ data.attributes }}
         }
 )
 
@@ -34,21 +38,21 @@ dag_id={{ "'"~data.dag_id|string~"'" }},
 def data_generator():
     meta_data = providers.get_active_meta_data()
     stream = dag.params.get('stream')
-    schema = stream['schema']
+    attributes = dag.params.get('attributes')
     data_frame = pd.DataFrame()
-    for scheme in schema:
-        (data, column) = tuple(scheme['type'].split('.'))
-        if data in meta_data['data_files']:
-            if column in meta_data['meta-headers'][data]:
-                # TODO-DataFrame Logic
-                LoggingMixin().log.info("OK...")
-            else:
-                raise AirflowException(f"TypeError: no data available for type {column} ")
-        else:
-            raise AirflowException(f"IOError: no data file found {data}.csv ")
+    for attrib in attributes:
+        df = pd.read_csv(f"{get_providers_home()}/{attrib}.csv", usecols=attributes[attrib])
+        data_frame[attributes[attrib]] = df[attributes[attrib]]
     
+    schema = stream['schema']
 
-
+    for scheme in schema:
+        field_name = scheme['field_name']
+        column_name = scheme['type'].split('.')[1]
+        data_frame.rename(columns={column_name:field_name}, inplace=True)
+    
+    data_frame.to_csv(f"{get_output_data_home()}/{stream['title']}.csv", index=False)
+    
 start = DummyOperator(task_id="start", dag=dag)
 end = DummyOperator(task_id="end", dag=dag)
 stream = PythonOperator(task_id="GenerateStream", python_callable=data_generator, dag=dag)
