c1183e4dc477e6324f2c13cec87296bf00a3f616
Shahbaz Ali <shahbaz.ali03@gmail.com>
2020-07-09 20:34:16 +0530

    add helper functions to get path of directory

--- system/dags/__init__.py
+++ system/dags/__init__.py
@@ -11,6 +11,7 @@ from airflow import settings
 from airflow.models.dag import DagModel
 from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.configuration import get_airflow_home
+from airflow.exceptions import AirflowException
 
 
 def get_cloudtdms_home():
@@ -29,6 +30,27 @@ def get_scripts_home():
     return f"{get_cloudtdms_home()}/scripts"
 
 
+def get_providers_home():
+    """
+    Returns `providers` directory path
+    :return:
+    """
+    return f"{get_airflow_home()}/cloudtdms/providers"
+
+
+def get_output_data_home():
+    """
+    Returns `data` directory path
+    :return:
+    """
+    path = f"{get_cloudtdms_home()}/data"
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    return path
+
+
+
 def delete_dag(dag_id):
     p = subprocess.Popen([f"airflow delete_dag -y {dag_id}"],executable="/bin/bash",
                          universal_newlines=True, shell=True)
@@ -38,6 +60,7 @@ def delete_dag(dag_id):
 sys.path.append(get_cloudtdms_home())
 
 from system.cloudtdms.utils.template import TEMPLATE
+from system.cloudtdms.providers import get_active_meta_data
 
 scripts = [f[:-3] for f in os.listdir(get_scripts_home()) if os.path.isfile(f"{get_scripts_home()}/{f}")
            and f.endswith('.py') and not f.startswith('__')]
@@ -61,12 +84,29 @@ for s in scripts:
 for (module, name) in modules:
     if hasattr(module, 'STREAM') and isinstance(getattr(module, 'STREAM'), dict):
         stream = getattr(module, 'STREAM')
+        meta_data = get_active_meta_data()
+        schema = stream['schema']
+        attributes = {}
+        for scheme in schema:
+            (data, column) = tuple(scheme['type'].split('.'))
+            if data in meta_data['data_files']:
+                if column in meta_data['meta-headers'][data]:
+                    if data not in attributes:
+                        attributes[data] = [column]
+                    else:
+                        attributes[data].append(column)
+                else:
+                    raise AirflowException(f"TypeError: no data available for type {column} ")
+            else:
+                raise AirflowException(f"IOError: no data file found {data}.csv ")
+
         template = Template(TEMPLATE)
         output = template.render(
             data={
                 'dag_id': str(name),
                 'frequency': stream['frequency'],
-                'stream' : stream
+                'stream' : stream,
+                'attributes' : attributes
             }
         )
         dag_file_path = f"{get_airflow_home()}/dags/{name}.py"
