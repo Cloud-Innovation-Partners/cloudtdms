b96d114afa0664e376a8854e8bcf57cdaedf712b
mdakeel <mdakeel.adeel@gmail.com>
2020-08-24 16:39:38 +0530

    adds more encryption techniques to encrypt method

--- system/cloudtdms/providers/advanced/__init__.py
+++ system/cloudtdms/providers/advanced/__init__.py
@@ -1,13 +1,14 @@
 #  Copyright (c) 2020. Cloud Innovation Partners (CIP)
 #  CloudTDMS - Test Data Management Service
-
+import hashlib
 import re
 import os
 import random
 import pandas as pd
 import numpy as np
 import base64
-
+import onetimepad
+from Crypto.Cipher import AES
 from airflow.utils.log.logging_mixin import LoggingMixin
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
@@ -53,9 +54,6 @@ def custom_list(data_frame, number, args=None):
 def set_null(data_frame, column):
     data_frame[column]=np.nan
 
-
-
-
 def get_key(custom_key):
     digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
     digest.update(custom_key)
@@ -68,9 +66,72 @@ def encryption(record, key):
     # decoded_text = cipher_suite.decrypt(encoded_text)
 
 
-def encrypt(data_frame, column, key):
-    data_frame[column]=data_frame[column].apply(encryption, key=key)
+def caesar(record, key):
+    result = ""
+    key=int(key)
+    # transverse the plain text
+    for i in range(len(record)):
+        char = record[i]
+        # Encrypt uppercase characters in plain text
 
+        if (char.isupper()):
+            result += chr((ord(char) + key - 65) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + key - 97) % 26 + 97)
+    return result
+
+def monoalpha_cipher(record):
+    monoalpha_cipher_list = {'a': 'm', 'A': 'M', 'b': 'n', 'B': 'N', 'c': 'b', 'C': 'B', 'd': 'v', 'D': 'V', 'e': 'c', 'E': 'C', 'f': 'x', 'F': 'X', 'g': 'z', 'G': 'Z', 'h': 'a', 'H': 'A', 'i': 's', 'I': 'S', 'j': 'd', 'J': 'D', 'k': 'f', 'K': 'F', 'l': 'g', 'L': 'G', 'm': 'h', 'M': 'H', 'n': 'j', 'N': 'J', 'o': 'k', 'O': 'K', 'p': 'l', 'P': 'L', 'q': 'p', 'Q': 'P', 'r': 'o', 'R': 'O', 's': 'i', 'S': 'I', 't': 'u', 'T': 'U', 'u': 'y', 'U': 'Y', 'v': 't', 'V': 'T', 'w': 'r', 'W': 'R', 'x': 'e', 'X': 'E', 'y': 'w', 'Y': 'W', 'z': 'q', 'Z': 'Q', ' ': ' '}
+    updated_record=''
+    for i in record:
+        updated_record+= monoalpha_cipher_list.get(i,i)
+    return updated_record
+
+def onetime(record, key):
+    record =str(record)
+    cipher = onetimepad.encrypt(record, key)
+    return cipher
+
+#AES
+    # pad with spaces at the end of the text
+    # beacuse AES needs 16 byte blocks
+def pad(key):
+    block_size = 16
+    remainder = len(key) % block_size
+    padding_needed = block_size - remainder
+    return key + padding_needed * ' '
+
+salt = b'J\x12\xeb0\xc0\x1c_\xee"\xdd\x95\x13\x17\xe5F\xe0'
+iv = b'\x84S\\__\x83\x1fG,\x17\xc1W\xc7\xd0$\xb8'
+
+def encrypt_aes(record, key):
+    record=str(record)
+    # use the Scrypt KDF to get a private key from the password
+    private_key = hashlib.scrypt(key.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
+
+    # pad text with spaces to be valid for AES CBC mode
+    padded_record = pad(record)
+
+    # create cipher config
+    cipher_config = AES.new(private_key, AES.MODE_CBC, iv)
+
+    # return a dictionary with the encrypted text
+    return cipher_config.encrypt(padded_record)
+
+def encrypt(data_frame, column, key, type):
+    if type == 'fernet':
+        data_frame[column]=data_frame[column].apply(encryption, key=key)
+    elif type == 'caesar':
+        data_frame[column] = data_frame[column].apply(caesar, key=key)
+    elif type == 'monoaplha':
+        data_frame[column] = data_frame[column].apply(monoalpha_cipher)
+    elif type == 'onetimepad':
+        data_frame[column] = data_frame[column].apply(onetime, key=key)
+    elif type == 'aes':
+        data_frame[column] = data_frame[column].apply(encrypt_aes, key=key)
+    else:
+        LoggingMixin().log.error("Invalid encryption type in attributes")
 
 def masking(record, with_, character, from_):
     record = str(record)
@@ -89,8 +150,10 @@ def masking(record, with_, character, from_):
         if len(record) < characters:
             record = with_ * len(record)
         else:
-            mid = int(len(record) / 2)
-            sub_part = record[:mid - 1] + with_ * characters
+            record_length = len(record)
+            remaining = record_length - int(characters)
+            right = int(remaining / 2)
+            sub_part = record[:right] + with_ * characters
             record = sub_part + record[len(sub_part):]
     else:
         LoggingMixin().log.error("Invalid masking attributes...")
@@ -144,7 +207,8 @@ def custom_file(data_frame, number, args=None):
 
         if 'encrypt' in args.get(column):
             key=args.get(column).get('encrypt').get('key')
-            encrypt(data_frame, column, key)
+            type=args.get(column).get('encrypt').get('type','fernet').lower()
+            encrypt(data_frame, column, key, type)
         elif 'shuffle' in args.get(column):
             shuffle_value=args.get(column).get('shuffle').lower()
             shuffle_value = True if shuffle_value == 'true' else False
