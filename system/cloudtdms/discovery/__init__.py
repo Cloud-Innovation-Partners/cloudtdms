b9da748820b7cc0bed38e9f8876132d32707d17f
mdakeel <mdakeel.adeel@gmail.com>
2020-09-04 13:49:23 +0530

    adds function call for location PII

--- system/cloudtdms/discovery/__init__.py
+++ system/cloudtdms/discovery/__init__.py
@@ -3,11 +3,13 @@
 
 import system.cloudtdms.discovery.person_name
 import system.cloudtdms.discovery.person_detail
+import system.cloudtdms.discovery.location
 
 
 def discover(data_frame):
     t_data_frame = data_frame.copy()
     return {
         "person_name": person_name.search(t_data_frame),
-        "person_detail": person_detail.search(t_data_frame)
+        "person_detail": person_detail.search(t_data_frame),
+        "location_detail": location.search(t_data_frame)
     }
