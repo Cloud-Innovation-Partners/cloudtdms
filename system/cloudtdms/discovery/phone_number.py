b2464810b6e9f014fd8ed48a49442a7051a0e78c
mdakeel <mdakeel.adeel@gmail.com>
2020-09-08 17:56:10 +0530

    adds phone_number module

---
+++ system/cloudtdms/discovery/phone_number.py
@@ -0,0 +1,41 @@
+#  Copyright (c) 2020. Cloud Innovation Partners (CIP)
+#  CloudTDMS - Test Data Management Service
+import re
+
+phone_sensitive_column_headers = ['phone_number', 'phone number', 'contact', 'contact_number','contact number','number']
+
+
+def phone_number_search_on_column_basis(data_frame, matched):
+    column_headers = data_frame.columns
+    matched_columns = [f for f in column_headers if f in phone_sensitive_column_headers]
+    return matched_columns
+
+def _is_valid_phone_number(number):
+    regex_phone_number="^((\+)?([0-9]{,3}))?(-|\s)?(\()?[0-9]{3}(\))?(-|\s)?[0-9]{3}(-|\s)?[0-9]{4}$"
+    pattern = re.compile(regex_phone_number)
+    return True if pattern.match(number) else False
+
+def phone_number_search_on_data_basis(data_frame, matched):
+    try:
+        data_frame.drop(matched, inplace=True, axis=1)
+    except KeyError:
+        print("No columns available for drop operation!")
+
+    data_frame = data_frame[data_frame.columns[(data_frame.applymap(type) == str).all(0)]]
+
+    columns = data_frame.columns
+    print(f'COLS ARE {columns}')
+    # Load Sample Data
+    statistic_match = []
+    for column in columns:
+        mask = data_frame[column].apply(_is_valid_phone_number)
+        if mask.sum() > 50:
+            statistic_match.append(column)
+
+    return statistic_match
+
+def search(data_frame):
+    result = []
+    result = phone_number_search_on_column_basis(data_frame, result)
+    result += phone_number_search_on_data_basis(data_frame, result)
+    return result
