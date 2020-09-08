21eac19ddf83031973b930a575657804c26b2e4a
mdakeel <mdakeel.adeel@gmail.com>
2020-09-08 17:52:47 +0530

    add function call for discovery methods

--- system/cloudtdms/discovery/__init__.py
+++ system/cloudtdms/discovery/__init__.py
@@ -4,12 +4,15 @@
 import system.cloudtdms.discovery.person_name
 import system.cloudtdms.discovery.person_detail
 import system.cloudtdms.discovery.location
+from system.cloudtdms.discovery import networking, phone_number
 
 
 def discover(data_frame):
     t_data_frame = data_frame.copy()
     return {
-        "person_name": person_name.search(t_data_frame),
-        "person_detail": person_detail.search(t_data_frame),
-        "location_detail": location.search(t_data_frame)
+        # "person_name": person_name.search(t_data_frame),
+        # "person_detail": person_detail.search(t_data_frame),
+        # "location_detail": location.search(t_data_frame),
+        "network_details":networking.search(t_data_frame),
+        "phone_number_details":phone_number.search(t_data_frame)
     }
